33accd8f985ba3c733d71398834881f1c64c6844
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-02-26 17:29:45 +0700

    chinh lai role của register, cập nhật thêm gender là enum

--- src/main/java/com/skincare_booking_system/SkincareBookingSystemApplication.java
+++ src/main/java/com/skincare_booking_system/SkincareBookingSystemApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkincareBookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkincareBookingSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SkincareBookingSystemApplication.class, args);
+    }
 }
