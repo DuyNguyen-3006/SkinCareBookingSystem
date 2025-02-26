01aaaeeea4fd87cab54c47c2e05ba0d894471fc1
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-02-26 22:43:32 +0700

    save

--- src/main/java/com/skincare_booking_system/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skincare_booking_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.skincare_booking_system.entity.Permission;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, String> {}
