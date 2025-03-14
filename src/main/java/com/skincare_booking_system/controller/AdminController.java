74b828bc7443ef596bef2b9448f4824316dac341
SkinCareBooking <kypase184884@fpt.edu.vn>
2025-03-15 03:47:08 +0700

    fix name function

--- src/main/java/com/skincare_booking_system/controller/AdminController.java
+++ src/main/java/com/skincare_booking_system/controller/AdminController.java
@@ -31,7 +31,7 @@ public class AdminController {
     }
 
     @GetMapping("/booking/total-money/month/{month}")
-    public ApiResponse<Double> totalMoneyAllSalonInMonth(@PathVariable int month) {
+    public ApiResponse<Double> totalMoneyAllSkincareServiceInMonth(@PathVariable int month) {
         return ApiResponse.<Double>builder()
                 .result(bookingService.totalMoneyByMonth(month))
                 .build();
