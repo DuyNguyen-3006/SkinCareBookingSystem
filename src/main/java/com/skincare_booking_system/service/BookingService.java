6aea306f86cf0d05693f7f06aea2a82af6900826
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-03-17 09:44:44 +0700

    commit file booking service and user

--- src/main/java/com/skincare_booking_system/service/BookingService.java
+++ src/main/java/com/skincare_booking_system/service/BookingService.java
@@ -311,7 +311,7 @@ public class BookingService {
             TherapistForBooking therapistBooking = new TherapistForBooking();
             therapistBooking.setId(therapist.getId());
             therapistBooking.setFullName(therapist.getFullName());
-            therapistBooking.setImgUrl(therapist.getImgUrl());
+            therapistBooking.setImage(therapist.getImage());
             therapistBooking.setFeedbackScore(therapistService.calculateAverageFeedback(therapist.getId(), "2025-03"));
             therapistsForBooking.add(therapistBooking);
         }
@@ -979,7 +979,7 @@ public class BookingService {
         response.setTherapistId(booking.getTherapistSchedule().getTherapist().getId());
         response.setBookingDate(booking.getBookingDay());
         response.setVoucherId(
-                booking.getVoucher() != null ? booking.getVoucher().getVoucherId() : 0);
+                booking.getVoucher() != null ? booking.getVoucher().getVoucherId() : "");
         log.info("Returning updated booking response: {}", response);
         return response;
     }
