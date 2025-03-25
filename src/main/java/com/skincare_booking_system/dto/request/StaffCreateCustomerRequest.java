cbe6fb6e7df29e0949f3b6db28b86d9d78745cfd
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-03-25 22:05:54 +0700

    them attriubute cho create user by staff

--- src/main/java/com/skincare_booking_system/dto/request/StaffCreateCustomerRequest.java
+++ src/main/java/com/skincare_booking_system/dto/request/StaffCreateCustomerRequest.java
@@ -1,10 +1,13 @@
 package com.skincare_booking_system.dto.request;
 
+import com.skincare_booking_system.validator.GenderConstraint;
 import jakarta.validation.constraints.Pattern;
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -14,4 +17,7 @@ public class StaffCreateCustomerRequest {
 
     @Pattern(regexp = "(84|0[35789])\\d{8}\\b", message = "INVALID_PHONE")
     String phone;
+    @GenderConstraint(message = "GENDER_INVALID")
+    String gender;
+    LocalDate birthDate;
 }
