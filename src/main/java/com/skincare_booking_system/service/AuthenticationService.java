ebb434ee3b2a49434ce81902b7ff122e30c32a59
phnanhky16 <Kypase184884@fpt.edu.vn>
2025-03-28 01:20:43 -0700

    fix login

--- src/main/java/com/skincare_booking_system/service/AuthenticationService.java
+++ src/main/java/com/skincare_booking_system/service/AuthenticationService.java
@@ -84,6 +84,9 @@ public class AuthenticationService {
 
         if (therapistRepository.existsByUsername(username)) {
             Therapist therapist = therapistRepository.findTherapistByUsername(username);
+            if(!therapist.getStatus()) {
+                throw new AppException(ErrorCode.LOGIN_FAILED);
+            }
             if (!new BCryptPasswordEncoder(10).matches(request.getPassword(), therapist.getPassword())) {
                 throw new AppException(ErrorCode.LOGIN_FAILED);
             }
@@ -97,6 +100,9 @@ public class AuthenticationService {
 
         if (staffRepository.existsByUsername(username)) {
             Staff staff = staffRepository.findStaffByUsername(username);
+             if(!staff.getStatus()) {
+                 throw new AppException(ErrorCode.LOGIN_FAILED);
+             }
             if (!new BCryptPasswordEncoder(10).matches(request.getPassword(), staff.getPassword())) {
                 throw new AppException(ErrorCode.LOGIN_FAILED);
             }
