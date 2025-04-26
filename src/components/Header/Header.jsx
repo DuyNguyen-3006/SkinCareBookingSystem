c6c184da450e5f343a79ff9bbb7fc1be871175a9
DuyNguyen-3006 <155596767+DuyNguyen-3006@users.noreply.github.com>
2025-04-26 11:29:09 +0700

    complete

--- src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import { clearAuthData } from "@/utils/auth";
-
-const handleLogout = () => {
-  clearAuthData();
-  showToast.success("Logged out successfully");
-  router.push("/login");
-}; 
--- src/utils/verifyCartContext.js
+++
@@ -1,7 +0,0 @@
-// This is a temporary file to verify imports
-import { useCart, CartProvider } from '@/context/CartContext';
-
-export const testCartContext = () => {
-  // This function does nothing, it's just to verify the imports work
-  console.log('Cart imports verified:', useCart, CartProvider);
-}; 
